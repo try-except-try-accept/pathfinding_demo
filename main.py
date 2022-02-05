0740484b99d5257845027167a63148ff6913c12b
Chris Hall <31258080+chall168421@users.noreply.github.com>
2022-02-06 06:42:15 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+
+size = 6
+square_size 
+
+
+def draw_grid():
+
+
+
+
+def get_num(prompt):
+
+     num = input(prompt)
+
+     while not num.isdigit() or not (6 <= int(num) <= 25):
+          print("Invalid. Enter between 6 and 25.")
+          num = input(prompt)
+
+     return int(num)
+
+
+
+
+
+
+
+
+def main():
+
+     size = get_num("Enter grid size: ")
+
+     restart = False
+     plot_path = False
+
+     while not restart:
+          while not plot_path:
+               
+
+               
+
+     
