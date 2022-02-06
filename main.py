4d8810cc06c1eafe7b35aa8ef3615fa89e0ca57f
Chris Hall <31258080+chall168421@users.noreply.github.com>
2022-02-06 09:00:33 +0700

    connection between nodes, dodgy

--- main.py
+++ main.py
@@ -9,11 +9,53 @@ size = 6
 
 origin = False
 destination = False
+
+
 graph_change = False
 
 ## app_state ::= 0 (setup)  1 (plot)   2 (run)
 
 app_state = 0
+
+
+
+def get_nearest_square(x, y):
+     print("Clicked at ", x, y)
+     x = x - (x % square_size) - lost_width
+     y = y - (y % square_size)
+
+     print("Now at", x, y)
+
+     return x, y
+
+
+class Connection:
+
+     def __init__(self):
+
+          self.origin = None
+          self.destination = None
+          self.steps = []
+          self.drawn = False
+
+     def append(self, x, y):
+          print("appended", x, y)
+          self.steps.append((x, y))
+
+     def draw(self, t):
+
+          for x, y in self.steps:
+               if (x,y) == self.destination: continue
+               t.penup()
+               t.goto(x, y)
+               t.begin_fill()
+               for _ in range(4):
+                    t.forward(square_size)
+                    t.right(90)
+               t.end_fill()
+          self.drawn = True
+
+          
 
 
 class Node:
@@ -33,36 +75,104 @@ class Node:
           t.end_fill()
           self.drawn = True
 
+          
+
+     def add_connection(self, x, y):
+
+          if not self.connections or self.connections[-1].destination is None:
+               self.connections.append(Connection())
+
+          print("Node class")
+          most_recent_conn = self.connections[-1]
+
+          print("Most recent conn", most_recent_conn)
+
+          most_recent_conn.append(x, y)
+          
+          print("Why append not work")
+
+     def end_connection(self, x, y):
+          self.connections[-1].destination = (x, y)
+
+          
+          
+
+          
+
 class Graph:
 
      def __init__(self):
-          self.nodes = []
+          self.nodes = {}
+          self.most_recent_node = None
 
 
-     def add_node(self, x, y):
-          print(f"Clicked at {x} {y}")
-          x = x - (x % square_size) - lost_width
-          y = y - (y % square_size)
-          print(f"Floored to {x} {y}")
+     def set_node(self, x, y):
+          x, y = get_nearest_square(x, y)
+          node_connector.goto(x, y)
+          
+          if (x,y) not in self.nodes:
+               this_node = Node(x, y)
+               self.nodes[(x,y)] = (this_node)
+          else:
+               this_node = self.nodes[(x, y)]
+               
+          self.current_node = this_node
 
-          self.nodes.append(Node(x, y))
+          print("Current node is", x, y)
 
+          
 
      def draw_nodes(self):
-          t = Turtle()
+          node_t = Turtle()
+          node_t.hideturtle()
+          conn_t = Turtle()
+          conn_t.hideturtle()
           print("Made turtle")
-          t.color("green")
-          t.speed(0)
+          node_t.color("green")
+          node_t.speed(0)
+          conn_t.color("yellow")
+          conn_t.speed(0)
           print("speed turtle")
-          for node in self.nodes:
+          for node in self.nodes.values():
                if not node.drawn:
-                    node.draw(t)
+                    node.draw(node_t)
+
+               for connection in node.connections:
+
+                    if not connection.drawn:
+
+                         connection.draw(conn_t)
+
+                    
+
                
 
 
-def get_nearest_square(x_click, y_click):
-     
-     return x_click % square_size, y_click % square_size
+     def connect_nodes(self, to_x, to_y):
+
+          to_x, to_y = get_nearest_square(to_x, to_y)
+
+          if (to_x, to_y) in self.nodes:
+               print("End of connection")
+               self.current_node.end_connection(to_x, to_y)
+
+          else:
+               print("Extending connection")
+               self.current_node.add_connection(to_x, to_y)
+
+
+
+          
+
+                    
+
+                    
+
+          
+
+          
+               
+
 
      
                
@@ -104,9 +214,11 @@ def sel_node():
           destination, origin = False, False
      
 def handle_click(x, y):
+     
      global graph_change
      if app_state == 1:
-          graph.add_node(x, y)
+          node_connector.goto(x, y)
+          graph.set_node(x, y)
           print("adding node")
           graph_change = True
           graph.draw_nodes()
@@ -114,6 +226,7 @@ def handle_click(x, y):
 
 def draw_grid(square_size):
      t = Turtle()
+     t.hideturtle()
      t.speed(0)
      left, right = -W, W
      top, bottom = -H, H
@@ -181,6 +294,12 @@ screen.onkey(sel_node, "n")
 screen.onclick(handle_click)
 
 graph = Graph()
+
+node_connector = Turtle()
+
+node_connector.ondrag(graph.connect_nodes)
+
+node_connector.penup()
 
 
 if __name__ == "__main__":
